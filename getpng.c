4565874c88d1d5b8fc3a3a4583e4c17daf1a5f91
LNSSPsd <lnsspsd@madisoncollege.edu>
2018-08-29 08:57:29 +0800

    Fixed all bugs

--- getpng.c
+++ getpng.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <memory.h>
 
-char header[4]={0x89,0x50,0x4E,0x47};
-char end[4]={0xAE,0x42,0x60,0x82};
+unsigned char *header="\x89PNG\r";
+unsigned char *end="IEND\xae\x42\x60\x82";
 int loop=1;
 
 void main(int argc,char *argv[]){
@@ -11,35 +11,37 @@ if(argc!=2){
 printf("getpng <file>\n");exit(1);
 }
 FILE *file=fopen(argv[1],"rb");
-int gett;
+int gett=0;
 int done=0;
 char fn[128]={0};
 while(1){
 gett=fgetc(file);
 if(gett==EOF){exit(0);}
 if(gett==header[done]){
-done++;continue;
-if(done==4){
+done++;
+if(done==5){
 done=0;
 memset(fn,128,0);
-sprintf(fn,"%d.png",loop);
+sprintf(fn,"%s_%d.png",argv[1],loop);
 loop++;
 FILE *out=fopen(fn,"wb");
+fputc('\x89',out);fputc('P',out);fputc('N',out);fputc('G',out);fputc('\r',out);
 while(1){
 gett=fgetc(file);
 if(gett==EOF){printf("ERROR: EOF TOO FIRST\n");exit(3);}
 fputc(gett,out);
 if(gett==end[done]){
 done++;
-if(done==4){printf("Found png %d=>%d.png\n",loop-1,loop-1);fclose(out);break;}
+if(done==8){done=0;printf("Found png %d=>%s\n",loop-1,fn);fclose(out);break;}
 continue;
   }
 if(done!=0){done=0;}
 
    }
-   }
+   }continue;
   }
   if(done!=0){done=0;}
+
  }
  //if(done!=0){done=0;}
 }
